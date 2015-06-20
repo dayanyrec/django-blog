e5cc0bb628d87a29972506d0b0a5271612ec4131
Dayany Espindola <dayanyrec@gmail.com>
2015-06-20 12:27:30 -0300

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+print('Hello!')
+
+if 3 < 2:
+    print('if')
+elif 3 > 2:
+    print('elif')
+else:
+    print('else')
+    
+def hi(name):
+    print(name)
+    
+hi('Dayany')
+
+girls = ['Rachel', 'Monica', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    
+    
+for i in range(6):
+    print(i)
